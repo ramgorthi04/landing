18cf8a6a4e205c856a1f9e08c209117a7edc342f
Ram Gorthi <30302781+ramgorthi04@users.noreply.github.com>
2024-08-05 12:24:43 -0400

    re-built and removed blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://ramgorthi04.github.io',
+  base: '/landing',  
   integrations: [react(), tailwind()],
-  outDir: './docs'
+  outDir: './docs',
+  build: {
+    assets: 'astro'
+  }
 });
